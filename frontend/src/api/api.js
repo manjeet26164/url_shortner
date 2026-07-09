4ce7f3235ac30d4923fcfdeb2b0b263e696e33dc
manjeet <manjeetyadav26164@gmail.com>
2026-07-09 10:53:53 +0530

    api fix

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = '/api';
+const apiUrl = import.meta.env.VITE_API_URL || '';
+const baseURL = `${apiUrl}/api`;
 export const api = axios.create({
   baseURL,
   headers: {
